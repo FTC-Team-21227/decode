77281905dd99ddad2ddae237ae31f18c53580455
gragao314 <oafish1982@gmail.com>
2025-10-11 12:50:01 -0700

    meep meep auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MeepMeepAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MeepMeepAuton.java
@@ -1,4 +1,44 @@
 package org.firstinspires.ftc.teamcode;
 
-public class MMAuton {
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.DriveConstants;
+
+@Autonomous(name = "MeepMeepAuton", group = "Autonomous")
+public class MeepMeepAuton extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // starting pose = meep meep start pose
+        Pose2d startPose = new Pose2d(0, 0, 0);
+
+        // init drive
+        MecanumDrive drive = new MecanumDrive(hardwareMap, startPose);
+
+        // build same trajectory as your MeepMeep path
+        TrajectoryActionBuilder traj = drive.actionBuilder(startPose)
+                .lineToX(30)
+                .turn(Math.toRadians(90))
+                .lineToY(30)
+                .turn(Math.toRadians(90))
+                .lineToX(0)
+                .turn(Math.toRadians(90))
+                .lineToY(0)
+                .turn(Math.toRadians(90));
+
+        Action trajectoryAction = traj.build();
+
+        // Wait for the game to start
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        // Follow the trajectory
+        Actions.runBlocking(trajectoryAction);
+    }
 }
