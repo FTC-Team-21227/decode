bfee73161a8351c825323d0fd9faca8d192b929d
Grace Song <gracesong2019@gmail.com>
2025-11-11 11:42:35 -0800

    comment out lynx line

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -220,7 +220,7 @@ public class MecanumDrive {
     }
 
     public MecanumDrive(HardwareMap hardwareMap, Pose2d pose, Robot.Color color) {
-        LynxFirmware.throwIfModulesAreOutdated(hardwareMap);
+//        LynxFirmware.throwIfModulesAreOutdated(hardwareMap);
 
         for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
             module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
